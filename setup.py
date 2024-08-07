9dd87054b1e45f1cd3377aaf458621faef3d225a
mshipman <MARK113@mapfreusa.com>
2024-08-07 15:36:27 +0000

    rearrange directory structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='quante_carlo',
+      version='0.1',
+      description='The most efficent implementation of Monte Carlo in the world.',
+      url='https://quantecarlo.com',
+      author='sign-of-fourier',
+      author_email='markpshipman@yahoo.com',
+      license='MIT',
+      packages=['quante_carlo'],
+      zip_safe=False)
