e8270c741fe47035bbb135979701b773491e90c8
mshipman <MARK113@mapfreusa.com>
2024-08-07 16:16:39 +0000

    increment version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='quante_carlo',
-      version='0.1',
+      version='0.1.1',
       description='The most efficent implementation of Monte Carlo in the world.',
       url='https://quantecarlo.com',
       author='sign-of-fourier',
