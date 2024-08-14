3303d5809850f8997643f6293e53b1326c420561
EC2 Default User <ec2-user@ip-172-16-48-196.ec2.internal>
2024-08-14 16:30:18 +0000

    gunicorn severs for bo

--- quante_carlo/quante.py
+++ quante_carlo/quante.py
@@ -1,18 +1,23 @@
 import numpy as np
 import random
+import json
+import requests
 from scipy.stats import multivariate_normal
-from sklearn.gaussian_process import GaussianProcessRegressor 
+#from sklearn.gaussian_process import GaussianProcessRegressor 
 import pandas as pd
 import warnings
 import time
+import os
+from datetime import datetime
 
 class hp_tuning_session:
-    def __init__(self, model, layer_ranges, kernel, batch_sz, n_gpr_processors, n_processors):
+    def __init__(self, model, layer_ranges, batch_sz, n_gpr_processors, n_processors, log_file):
         self.gpr_batch_size = batch_sz
-        self.kernel = kernel
+        
         self.n_gpr_processors = n_gpr_processors
         self.model = model
         self.layer_ranges = layer_ranges
+        self.hp_types = ['int' if type(x[0]) == int else 'float' for x in layer_ranges]
         self.n_processors = n_processors
         self.multivariate = True
         self.qc = False
@@ -20,7 +25,16 @@ class hp_tuning_session:
         self.y_best = -1
         self.iteration_id = []
         self.ei_history = [-1]*n_processors
+        self.logfile = log_file
+        if not os.path.isfile(log_file):
+            with open(log_file, 'w') as f:
+                f.write("Starting\n")
 
+    def log(self, x):
+
+        dt = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+        with open(self.logfile, 'a') as f:
+            f.write('[' + dt + '] '+ x + "\n")
 
     def initialize_gpr(self, p, other_parameters):
         
@@ -43,29 +57,6 @@ class hp_tuning_session:
         return points
 
 
-
-
-    def qei(self, batch_id):
-        
-        best_ids = []
-        best_ccdf = 0
-        
-        for batch in self.batch_points[batch_id]:
-            
-            mu, sigma = self.gpr.predict(batch, return_cov=True)
-        
-            if self.multivariate:
-            #    if self.qc:
-            #        ccdf = 1 - qc([p['y_best']] * p['q'] - p['mu'][d], [p['sigma'][q][d] for q in d])
-            #    else:
-                ccdf = 1 - multivariate_normal.cdf([self.y_best]*self.n_processors, mu, sigma)
-            #else:
-            #ccdf = 1 - np.prod([norm.cdf(p['y_best'], p['mu'][d[q]], p['sigma'][d[q]][d[q]]) for q in range(p['q'])])
-            if ccdf > best_ccdf:
-                best_ccdf = ccdf
-                best_ids = batch
-                
-        return best_ccdf, best_ids
     
     def test_new_points(self, p, other_parameters):
         
@@ -79,27 +70,38 @@ class hp_tuning_session:
         
         self.score_history += self.scores
         self.y_best = max([np.mean(s) for s in self.score_history])
-        
+        pd.DataFrame({'scores': self.score_history, 'points': [','.join([str(x) for x in h]) for h in self.layer_history]}).to_csv('history.txt', index=False)
+
+
+
+
     def get_new_points(self, p):
         
-        self.gpr = GaussianProcessRegressor(kernel=self.kernel,random_state=0)
-        with warnings.catch_warnings():
-            warnings.simplefilter('ignore')
-            self.gpr.fit(self.layer_history, [np.mean(x) for x in self.score_history])
 
-        # optimize krig model
-        self.batch_points = [[self.get_random_points(self.n_processors) for n in range(self.gpr_batch_size)] for g in range(self.n_gpr_processors)]
-        batch = p.map(self.qei, range(self.n_gpr_processors))
-        best_ccdf = -1
-        for i, j in batch:
-            if i > best_ccdf:
-                best_ccdf = i
-                best_ids = j
+        hp_ranges = ';'.join([','.join([str(x) for x in s]) for s in self.layer_ranges])
+        hp_types = ','.join(self.hp_types)
+        
+        #stem = 'http://localhost:8000/bayes_opt?hp_types='+hp_types+'&g_batch_size='+str(self.gpr_batch_size)+'&layer_ranges='+hp_ranges
+        url = "http://localhost:8000/bayes_opt?hp_types={}&g_batch_size={}&layer_ranges={}&y_best={}&n_gpus={}".format(
+                hp_types, 
+                self.gpr_batch_size, 
+                hp_ranges, 
+                self.y_best,
+                self.n_processors)
+        #urls = [stem + str(i) + '&y_best='+str(self.y_best) for i in range(self.n_gpr_processors)]
+        worker_results = p.map(requests.get, [url] * self.n_gpr_processors)
+        jsponse = [json.loads(a.content.decode('utf-8')) for a in worker_results]
+        best_score = -1
+        for score, points in zip([j['best_ccdf'] for j in jsponse], [j['next_points'] for j in jsponse]):
+            if score > best_score:
+                best_score = score
+                best_points = points
 
-        #best_ccdf, best_ids = self.qei(gpr, p)
-        self.ei_history += [best_ccdf]*self.n_processors
-        self.next_points = best_ids
-        self.layer_history += best_ids
+        self.next_points = [tuple([int(x) for x in s.split(',')]) for s in best_points.split(';')]
+        self.layer_history += self.next_points
+        self.ei_history += [best_score]*self.n_processors
+
+        return 1
   
     def set_iteration_id(self, iteration_history):
         self.iteration_id = iteration_history
@@ -115,22 +117,23 @@ class hp_tuning_session:
             
 
 
-def carlo(f, limits, kernel, gpr_batch_size, n_gpr_processors, n_processors, n_iterations, other_parameters={}):
+def carlo(f, limits, gpr_batch_size, n_gpr_processors, n_processors, n_iterations, other_parameters={}, log_file='/tmp/qclog_file.txt'):
     def qc_tune_nn(p):
 
-        q = hp_tuning_session(f, limits, kernel, gpr_batch_size, n_gpr_processors, n_processors)
+        q = hp_tuning_session(f, limits, gpr_batch_size, n_gpr_processors, n_processors, log_file)
         q.initialize_gpr(p, other_parameters)
         iteration_id = [0] * n_processors
 
         for j in range(n_iterations):
-            print(j)
+            q.log("iteration {}".format(j))
             start = time.time()
             q.get_new_points(p)
-            print("  {} seconds getting next points".format(round(time.time()-start, 2)))
+            q.log("- {} seconds getting next points".format(round(time.time()-start, 2)))
             start = time.time()
             q.test_new_points(p, other_parameters)
-            print("  {} seconds testing next points".format(round(time.time()-start, 2)))
+            q.log("- {} seconds testing next points".format(round(time.time()-start, 2)))
             iteration_id += [j+1]*n_processors
+            q.log("- current best {}".format(q.y_best))
 
         q.set_iteration_id(iteration_id)
         return q
