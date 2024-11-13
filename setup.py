12d13cccd395d3b9f07bd53898a4cc65819ff319
mshipman <MARK113@mapfreusa.com>
2024-11-13 00:44:17 +0000

    setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='quante_carlo',
-      version='0.1.4',
+      version='0.1.5',
       description='The most efficent implementation of Monte Carlo in the world.',
       url='https://quantecarlo.com',
       author='sign-of-fourier',
